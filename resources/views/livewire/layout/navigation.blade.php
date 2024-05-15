1362f8f137593502cd7a62fe9cc7b2f261814ab0
Yassett <samirmorales482@gmail.com>
2024-05-15 17:51:10 -0600

    Agregado de botón en responsive Admin Panel

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -92,6 +92,12 @@ new class extends Component
             </x-responsive-nav-link>
         </div>
 
+        <div class="pt-2 pb-3 space-y-1">            
+            <x-responsive-nav-link :href="route('adminPanel')" :active="request()->routeIs('adminPanel')" wire:navigate>
+                {{ __('Admin Panel') }}
+            </x-responsive-nav-link>
+        </div>
+
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
             <div class="px-4">
